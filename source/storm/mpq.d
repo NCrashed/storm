9d322b51090822a5826584869a3ffe96e9abacec
NCrashed <NCrashed@gmail.com>
2014-05-03 03:55:47 +0400

    Mpq module

---
+++ source/storm/mpq.d
@@ -0,0 +1,7 @@
+/**
+*   Copyright: © 1999-2014 Ladislav Zezula
+*   License: Subject to the terms of the MIT license, as written in the included LICENSE file.
+*   Authors: Ladislav Zezula, NCrashed <ncrashed@gmail.com>
+*/
+module storm.mpq;
+
